856b55d12a16a1895e8a0ff68b9a74fd28c4727b
Biju S Nair <bijusalapatt@gmail.com>
2017-05-08 11:40:22 +0000

    Commit after getting most of the repository fields.  need to get Branch and Commit info...

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 import pymongo
 from github import Github
+from bitbucket.bitbucket import  Bitbucket
 
-#from pybitbucket import bitbucket
+AUTH_USER='bijualappatt'
+AUTH_PASS='Password@123'
 
-MongoDBServer = 'localhost'
-MongoDBPort = 27017
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017
 
-client = pymongo.MongoClient(MongoDBServer, MongoDBPort)
+client = pymongo.MongoClient(MONGODB_SERVER, MONGODB_PORT)
 db = client.testDB
 
-github = Github('bijualappatt', 'Password@123')
-# user = github.get_user()
+github = Github(AUTH_USER, AUTH_PASS)
 
 for repo in github.get_user().get_repos():
-    print(repo.id)
-    print(repo.name)
-    print(repo.full_name)
-    print(repo.description)
-    print(repo.clone_url)
-    print(repo.downloads_url)
-    print(repo.owner)
 
-    # db.RepositoryInfo.insert_one(
-    #     {"RepositoryID": repo.id,
-    #      "RepositoryName": repo.name,
-    #      "RepoFullName": repo.full_name,
-    #      "RepoDescription": repo.description,
-    #      "CloneURL": repo.clone_url,
-    #      "DownloadURL": repo.downloads_url
-    #      }
-    # )
+    #   TODO: Check for duplication
+    #   TODO: Get Branch and Commit details
 
+    db.RepositoryInfo.insert_one(
+        {"RepositoryID": repo.id,
+         "RepositoryName": repo.name,
+         "FullName": repo.full_name,
+         "Description": repo.description,
+         "OwnerID":repo.owner.id,
+         "OwnerUserName":repo.owner.login,
+         "OwnerType":repo.owner.type,
+         "RepositoryURL":repo.url,
+         "BranchesURL":repo.branches_url,
 
+         "LanguagesURL":repo.languages_url,
+         "CommitsURL":repo.commits_url,
+         "IssuesURL": repo.issues_url,
+         "GitURL": repo.git_url,
+         "CloneURL": repo.clone_url,
+         "CreatedAt": repo.created_at,
+         "UpdatedAt":repo.updated_at,
+         "Size":repo.size,
+         "Language":repo.language,
+         "DefaultBranch":repo.default_branch,
+         "HassIssues":repo.has_issues,
+         "IsPrivate":repo.private
+         }
+    )
 
 
